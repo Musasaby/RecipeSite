8d3ee6a9d4d2691f51cb5f46bdc8e6780102b1d4
TomoshigeKosei <22cu0224@jec.ac.jp>
2025-04-21 06:57:19 +0900

    ヘッダー固定

--- templates/header.php
+++ templates/header.php
@@ -7,8 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
-<body class="bg-light">
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+<body class="bg-light pt-5 mt-5"> <!-- パディング追加 -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top shadow-sm"> <!-- fixed-topとshadow-sm追加 -->
         <div class="container">
             <a class="navbar-brand" href="<?php echo $app->getPageUrl('home'); ?>"><?php echo htmlspecialchars($title); ?></a>
             <button
